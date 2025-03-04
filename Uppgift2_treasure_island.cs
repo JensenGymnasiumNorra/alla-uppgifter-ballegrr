f017134270ca0e0f0f3bc45f3d1081594397a5c7
ballegrr <lilgra070914@student.jenseneducation.se>
2025-03-04 12:56:50 +0000

    Done

--- Uppgift2_treasure_island.cs
+++ Uppgift2_treasure_island.cs
@@ -13,3 +13,53 @@ Inmatning
 If-satser
 
 */
+
+using System.Net.Security;
+
+class TreasureIslandGame
+{
+    static void Main()
+    {
+        Console.WriteLine("Välkommen till Skattkammarön. Ditt uppdrag är att finna skatten.");
+
+        Console.WriteLine("Vänster eller höger?");
+        string choice1 = Console.ReadLine().ToLower();
+
+        if (choice1 == "vänster")
+        {
+            Console.WriteLine("Simma eller vänta?");
+            string choice2 = Console.ReadLine().ToLower();
+
+            if (choice2 == "vänta")
+            {
+                Console.WriteLine("Vilken dörr? (Röd, Blå, Gul)");
+                string choice3 = Console.ReadLine().ToLower();
+
+                if (choice3 == "gul")
+                {
+                    Console.WriteLine("Du hittade skatten! Du vinner!");
+                }
+                else if (choice3 == "röd")
+                {
+                    Console.WriteLine("Ihjälbränd. Game over.");
+                }
+                else if (choice3 == "blå")
+                {
+                    Console.WriteLine("Uppäten av bestar. Game over.");
+                }
+                else
+                {
+                    Console.WriteLine("Game over.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Uppäten av fiskar. Game over.");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Ramlade ner i ett hål. Game over.");
+        }
+    }
+}
